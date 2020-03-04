36bb6ef351aa7cd4d9e1599743e8f34fd6394448
kendyjaky@gmail.com <you@example.com>
2020-03-05 02:44:26 +0300

    Added more images and Animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:indoor_floral_plants/animations/FadeAnimation.dart';
 
 void main() => runApp(
   MaterialApp(
@@ -19,88 +20,117 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            height: 300,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/images/background.png'),
-                  fit: BoxFit.cover
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/background.png'),
+                    fit: BoxFit.cover
+                ),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.bottomRight,
+                        colors: [
+                          Colors.black.withOpacity(.8),
+                          Colors.black.withOpacity(.2),
+                        ]
+                    )
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    FadeAnimation(1, Text("Here to bring you the best indoor flowers for your home decor",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.yellowAccent,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),)),
+                    SizedBox(height: 30,),
+                    FadeAnimation(1.3, Container(
+                      padding: EdgeInsets.symmetric(vertical: 3),
+                      margin: EdgeInsets.symmetric(horizontal: 40),
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            prefixIcon: Icon(Icons.search, color: Colors.blue,),
+                            hintStyle: TextStyle(
+                                color: Colors.grey, fontSize: 15),
+                            hintText: "Search for Flowers..."
+                        ),
+                      ),
+                    )),
+                    SizedBox(height: 30,)
+                  ],
+                ),
               ),
             ),
-            child: Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.bottomRight,
-                      colors: [
-                        Colors.black.withOpacity(.8),
-                        Colors.black.withOpacity(.3 ),
-                      ]
-                  )
-              ),
+            SizedBox(height: 30,),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text("Bringing the light of flowers right to your device!", textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.yellowAccent,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold),),
-                  SizedBox(height: 30,),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 3),
-                    margin: EdgeInsets.symmetric(horizontal: 40),
-                    height: 50,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.white
+                  FadeAnimation(1, Text("Flowering Plants", style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[800],
+                      fontSize: 20),)),
+                  SizedBox(height: 20,),
+                  FadeAnimation(1.4, Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        makeItem(
+                            image: 'assets/images/madagascarJ.jpg', title: 'Madagascar JAsmine'),
+                        makeItem(
+                            image: 'assets/images/ornamentalP.jpg', title: 'Ornamenta Pepper'),
+                        makeItem(
+                            image: 'assets/images/flamingK.jpg', title: 'Flaming Katy'),
+                        makeItem(image: 'assets/images/mothO.jpeg',
+                            title: 'Moth Orchid')
+                      ],
                     ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          prefixIcon: Icon(Icons.search, color: Colors.blue,),
-                          hintStyle: TextStyle(color: Colors.deepPurple, fontSize: 20),
-                          hintText: "Serch for flowers..."
-                      ),
+                  )),
+
+                  SizedBox(height: 20,),
+                  FadeAnimation(1, Text("Succulent Plant", style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[800],
+                      fontSize: 20),)),
+                  SizedBox(height: 20,),
+                  FadeAnimation(1.4, Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        makeItem(image: 'assets/images/snake.jpg',
+                            title: 'Snake Plant'),
+                        makeItem(
+                            image: 'assets/images/aloe.jpg', title: 'Aloe Vera'),
+                        makeItem(
+                            image: 'assets/images/motherO.jpeg', title: 'Mother of Thousands'),
+                        makeItem(image: 'assets/images/panda.jpg',
+                            title: 'Panda Plant')
+                      ],
                     ),
-                  ),
-                  SizedBox(height: 10,)
+                  )),
+                  SizedBox(height: 80,),
                 ],
               ),
-            ),
-          ),
-          SizedBox(height: 30,),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text("Flowering Plants", style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.greenAccent,
-                    fontSize: 20),),
-                SizedBox(height: 20,),
-                Container(
-                  height: 200,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: <Widget>[
-                      makeItem(image: 'assets/images/flamingK.jpg', title: 'Flaming Katy'),
-                      makeItem(image: 'assets/images/mothO.jpeg', title: 'Moth Ochid'),
-                      makeItem(image: 'assets/images/madagascarJ.jpg', title: 'Madagascar Jasmine'),
-                      makeItem(image: 'assets/images/ornamentalP.jpg', title: 'Ornamental Pepper'),
-                    ],
-                  ),
-                ),
-
-              ],
-            ),
-          )
-
-        ],
-      )
-
+            )
+          ],
+        ),
+      ),
     );
   }
 
