0f9c7fd8c6a048dade422e9ab57a05966f191c23
Jacqueline Kendi <kendyjaky@gmail.com>
2021-04-24 16:14:26 +0300

    App taking shape

--- lib/pages/screens/flowering.dart
+++ lib/pages/screens/flowering.dart
@@ -14,15 +14,76 @@ class _FloweringPlantsState extends State<FloweringPlants> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.amber,
+        backgroundColor: Colors.white,
+        elevation: 0,
         title: Text(
           AppStrings.flowering, 
           style: AppTextStyle.textSize20.copyWith(color: AppColors.primaryColor),
         ),
       ),
-      body: Scaffold(
-        
-      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.yellow,
+              height: 100,
+              width: double.infinity,
+              child: Text("My Plants"),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 200,
+                  width: 300,
+                  color: Colors.tealAccent,
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 200,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage('assets/images/lady.jpg')),
+                    color: Colors.blueGrey,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
+                    )
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 200,
+                  width: 300,
+                  color: Colors.tealAccent,
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 200,
+                  width: 300,
+                  color: Colors.tealAccent,
+                ),
+              ],
+            ),
+          ],
+        ),
+      )
       
     );
   }
